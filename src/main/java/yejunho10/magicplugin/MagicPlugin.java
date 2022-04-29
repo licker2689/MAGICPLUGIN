1ea3dd6ab04b6084e38ae10b3cbd08ec87636db7
licker2689 <83544855+licker2689@users.noreply.github.com>
2022-04-29 23:36:47 +0900

    Initial commit

---
+++ src/main/java/yejunho10/magicplugin/MagicPlugin.java
@@ -0,0 +1,17 @@
+package yejunho10.magicplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MagicPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
